9abebdb70433c8813092108f4c227e446455d55e
leonlux <leonlux@icloud.com>
2024-03-22 16:12:06 +0100

    add function to send activation mail to user

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod auth;
 pub mod config;
 pub mod entity;
+pub mod mail;
 pub mod routes;
 pub mod setup;
---
+++ src/mail.rs
@@ -0,0 +1,35 @@
+use sendgrid::v3::{Content, Email, Message, Personalization, Sender};
+use uuid::Uuid;
+
+pub async fn send_registration_mail(
+    user_id: &Uuid,
+    mail: &String,
+    base_url: String,
+    api_key: String,
+) -> Result<(), anyhow::Error> {
+    let p = Personalization::new(Email::new("pastr@1ux.dev"));
+
+    let msg = Message::new(Email::new(mail))
+        .set_subject("Pastr Registration")
+        .add_content(
+            Content::new()
+                .set_content_type("text/html")
+                .set_value(format!(
+                    r#"Hello,
+                    
+                    You signed up for pastr, but your account needs to be activated.
+                    
+                    Simply click on this link: {}/activate?id={}"#,
+                    base_url, user_id,
+                )),
+        )
+        .add_personalization(p);
+
+    let sender = Sender::new(api_key);
+    let response = sender.send(&msg).await?;
+    if response.status() != http::StatusCode::ACCEPTED {
+        Err(anyhow::anyhow!("error sending mail to user"))
+    } else {
+        Ok(())
+    }
+}
