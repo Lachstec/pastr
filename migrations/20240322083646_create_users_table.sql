2e86c2367ab27d8ffc0251aad40601a490e3c156
leonlux <leonlux@icloud.com>
2024-03-22 15:34:09 +0100

    add migrations for registering users

--- migrations/20240322083646_create_users_table.sql
+++ migrations/20240322083646_create_users_table.sql
@@ -2,7 +2,7 @@ CREATE EXTENSION citext;
 
 -- Domain that validates mail addresses via a regular expression
 -- Not 100% correct, but works 90% of the time.
-CREATE DOMAIN IF NOT EXISTS email AS citext
+CREATE DOMAIN email AS citext
     CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
 
 CREATE TABLE IF NOT EXISTS pastr.users(
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS pastr.users(
     username TEXT NOT NULL,
     mail email NOT NULL,
     password_hash TEXT NOT NULL,
+    enabled BOOLEAN,
     
     UNIQUE(username),
-    UNIQUE(mail),
+    UNIQUE(mail)
 );
---
+++ migrations/20240322135952_user_account_confirmation.sql
@@ -0,0 +1,20 @@
+-- Signup Request that stores the id of the unactivated user account.
+-- Requests get deleted via trigger when they are older than 2 days.
+CREATE TABLE IF NOT EXISTS pastr.users_confirmations (
+    user_id uuid NOT NULL REFERENCES pastr.users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- Deletes rows older than 2 days via a trigger.
+CREATE FUNCTION users_cofirmations_delete_expired() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    DELETE FROM pastr.users_confirmations WHERE created_at < NOW() - INTERVAL '2 days';
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER users_confirmations_delete_expired_trigger
+    AFTER INSERT ON pastr.users_confirmations
+    EXECUTE PROCEDURE users_cofirmations_delete_expired();
