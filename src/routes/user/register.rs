b8a650498a79d050364be4927b32b08a4c1d4393
Leon Lux <leonlux@icloud.com>
2024-04-09 22:12:18 +0200

    remove baseurl from template

--- src/routes/user/register.rs
+++ src/routes/user/register.rs
@@ -1,18 +1,11 @@
-use crate::setup::AppBaseUrl;
-use actix_web::web;
 use actix_web_lab::respond::Html;
 use askama::Template;
 
 #[derive(Template)]
 #[template(path = "register.html")]
-struct RegistrationPage {
-    pub base_url: String,
-}
+struct RegistrationPage;
 
-pub async fn register(base_url: web::Data<AppBaseUrl>) -> Html {
-    let page = RegistrationPage {
-        base_url: base_url.0.clone(),
-    };
-    let html = page.render().unwrap();
+pub async fn register() -> Html {
+    let html = RegistrationPage.render().unwrap();
     Html(html)
 }
