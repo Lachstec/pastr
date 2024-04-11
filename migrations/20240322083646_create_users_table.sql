1993485b907277fa4fb0068223075dfda3c9487e
Leon Lux <leonlux@icloud.com>
2024-04-11 14:52:04 +0200

    reformat sql file

--- migrations/20240322083646_create_users_table.sql
+++ migrations/20240322083646_create_users_table.sql
@@ -1,17 +1,15 @@
 CREATE EXTENSION citext;
-
 -- Domain that validates mail addresses via a regular expression
 -- Not 100% correct, but works 90% of the time.
-CREATE DOMAIN email AS citext
-    CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
-
+CREATE DOMAIN email AS citext CHECK (
+    value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
+);
 CREATE TABLE IF NOT EXISTS pastr.users(
     id uuid PRIMARY KEY,
     username TEXT NOT NULL,
     mail email NOT NULL,
     password_hash TEXT NOT NULL,
     enabled BOOLEAN,
-    
     UNIQUE(username),
     UNIQUE(mail)
 );
