1e8244badf3feee47f81d22bca4794bed64f72de
Leon Lux <leonlux@icloud.com>
2024-04-11 14:51:55 +0200

    update trigger to correctly delete user accounts that are not activated

--- migrations/20240322135952_user_account_confirmation.sql
+++ migrations/20240322135952_user_account_confirmation.sql
@@ -4,17 +4,19 @@ CREATE TABLE IF NOT EXISTS pastr.users_confirmations (
     user_id uuid NOT NULL REFERENCES pastr.users(id),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
 -- Deletes rows older than 2 days via a trigger.
-CREATE FUNCTION users_cofirmations_delete_expired() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-    DELETE FROM pastr.users_confirmations WHERE created_at < NOW() - INTERVAL '2 days';
-    RETURN NEW;
+CREATE FUNCTION users_cofirmations_delete_expired() RETURNS trigger LANGUAGE plpgsql AS $$ BEGIN
+DELETE FROM pastr.users
+WHERE id IN (
+        SELECT user_id
+        FROM pastr.users_confirmations
+        WHERE created_at < NOW() - INTERVAL '2 days'
+    )
+DELETE FROM pastr.users_confirmations
+WHERE created_at < NOW() - INTERVAL '2 days';
+RETURN NEW;
 END;
 $$;
-
 CREATE TRIGGER users_confirmations_delete_expired_trigger
-    AFTER INSERT ON pastr.users_confirmations
-    EXECUTE PROCEDURE users_cofirmations_delete_expired();
+AFTER
+INSERT ON pastr.users_confirmations EXECUTE PROCEDURE users_cofirmations_delete_expired();
